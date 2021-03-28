8938adfc762d261b00c076f227c87651fdc81738
Florian <florian.gaspar2000@gmail.com>
2021-03-28 16:40:03 +0200

    remainingSleepTime now working correctly

--- Thread.cpp
+++ Thread.cpp
@@ -4,13 +4,19 @@
 void Thread::Sleep(unsigned long ms)
 {
   sleepEndTime = millis() + ms;
+  threadState = Thread::SLEEPING;
 }
 
 unsigned long Thread::remainingSleepTime()
 {
   if(isSleeping())
   {
-    return  sleepEndTime - millis();
+    #ifdef DEBUG
+    Serial.print(name);
+    Serial.print(" sleeping for ");
+    Serial.println(sleepEndTime - millis());
+    #endif
+    return  sleepEndTime > millis() ? sleepEndTime - millis() : 0;
   }
   return 0;
 }
@@ -22,6 +28,10 @@ bool Thread::isSleeping()
 
 void Thread::Continue()
 {
+  #ifdef DEBUG
+  Serial.print(name);
+  Serial.println(" stopped sleeping");
+  #endif
   threadState = Thread::ACTIVE;
   sleepEndTime = 0;
 }
