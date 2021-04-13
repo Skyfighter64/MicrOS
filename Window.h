8e4059c60d2717c4e2e891263f5c7b2d3e1adb79
Florian <florian.gaspar2000@gmail.com>
2021-04-13 18:47:15 +0200

    Add UI documentation

--- Window.h
+++ Window.h
@@ -10,6 +10,32 @@
 
 /*
   Class representing an UI Window
+
+  How it works:
+
+  A window is a container for:
+
+    - UI elements
+    - clickables
+
+  Where UI Elements can be anything inheriting from "UIElement".
+  Whenever the Window gets drawn by the WindowManager, the "Draw()" function
+  of each UIElement of this window gets executed.
+
+  This results in those UIElements being drawn onto the screen.
+
+
+  Clickables are objects containing an "OnClick()" function and an InputID.
+
+  Whenever a button with the same InputID as a clickable gets pressed, the clickable's
+  "OnClick()" function will be automatically executed by the InputManager,
+  but ONLY IF the clickable is part of the currently active window.
+
+    | To switch the currently active window, simply call
+    | WindowManager::SetActiveWindow(...)
+
+  Therefore, each button can do different things depending on which window gets
+  displayed on the screen.
 */
 
 class Window : public UIElement
