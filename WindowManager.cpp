ee3c266faa21bd63a609d6b0300c298236e947df
Florian <florian.gaspar2000@gmail.com>
2022-03-03 16:52:36 +0100

    Move display updates to window manager

--- WindowManager.cpp
+++ WindowManager.cpp
@@ -9,15 +9,32 @@ WindowManager::WindowManager(U8G2 * _displayPtr) : displayPtr(_displayPtr), acti
 
 void WindowManager::Initialize()
 {
-  //nothing to do here...
+  InitializeDisplay();
 }
 
-/*
-    This function crashes when called
-*/
+
 void WindowManager::Run()
 {
-  //check if there are any windows
+  // draw the current window if possible
+  DrawWindow();
+  // update the display if possible
+  UpdateDisplay();
+
+  // limit the refresh rate of the display to boost overall performance
+  if(frameDelay > 0)
+  {
+    Sleep(frameDelay);
+  }
+}
+
+
+
+/**
+  function drawing the currently active window if possible
+*/
+void WindowManager::DrawWindow()
+{
+  //check if there is an active window to draw
   if(activeWindowPtr == nullptr)
   {
       //there is no window to display
@@ -26,6 +43,50 @@ void WindowManager::Run()
 
   //draw the currently active window
   activeWindowPtr->Draw(displayPtr);
+}
+
+
+/**
+  function initializing the display
+*/
+void WindowManager::InitializeDisplay()
+{
+  //check if a valid display was given
+  if (displayPtr == nullptr)
+  {
+      // no valid display given
+      return;
+  }
+
+
+  //initialize the display driver
+  displayPtr->begin();
+  // set the default font
+  displayPtr->setFont(u8g2_font_tinytim_tf);
+  // initialize window drawing
+  displayPtr->firstPage();
+}
+
+
+/**
+  function updating the display using the driver if possible
+*/
+void WindowManager::UpdateDisplay()
+{
+  //check if a valid display to draw to
+  if (displayPtr == nullptr)
+  {
+      // no valid display given
+      return;
+  }
+
+  // draw one chunk of the image on the screen
+  if(!displayPtr->nextPage())
+  {
+    // the end of the display was reached
+    // return to the top
+    displayPtr->firstPage();
+  }
 }
 
 
--- WindowManager.h
+++ WindowManager.h
@@ -8,13 +8,25 @@
 
 /*
   This thread handles and draws all windows
-  and provides functions to switch the displayed window
+
+  It also provides functions to switch the displayed window
+  and updates the display using the display driver
 */
 
 
 class WindowManager : public Thread
 {
   public:
+    // the delay in ms inbetween each frame drawn to the screen
+    // use to limit the framerate and boost overall performance
+    /*
+      To calculate the frame delay: e.g. desired frame rate = 60 FPS:
+      1000 / 60 Hz = 16,6 ms          -> Frame delay should be ~17ms
+      Note: This is not accurate if a page buffer is used by u8g2 (delay has to
+       be divided by page count additionally)
+    */
+    unsigned long frameDelay = 10;
+
     WindowManager( U8G2 * _displayPtr);
     void SetActiveWindow(Window * windowPtr);
     Window * GetActiveWindow();
@@ -29,6 +41,11 @@ class WindowManager : public Thread
 
     //the currently active window
     Window* activeWindowPtr;
+
+    void InitializeDisplay();
+    void UpdateDisplay();
+
+    void DrawWindow();
 };
 
 #endif
